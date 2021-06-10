05d4f5467ea38814df5dbbde301f504ce3fa7290
David-Lisboa <61177183+David-Lisboa@users.noreply.github.com>
2021-06-09 21:09:55 -0400

    Incio da implementação da mainActivetyi

--- app/src/main/java/com/davidpjoject/fucapiead/MainActivity.kt
+++ app/src/main/java/com/davidpjoject/fucapiead/MainActivity.kt
@@ -2,10 +2,15 @@ package com.davidpjoject.fucapiead
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
