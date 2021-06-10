8330706fcf81559e3e48e41575b923b21205d8e5
David-Lisboa <61177183+David-Lisboa@users.noreply.github.com>
2021-06-09 20:10:22 -0400

    Login tela implementada
    Inserido os campos email, senha e botão login.
    Criado passagem de tela ao clicar no botão login

--- app/src/main/java/com/davidpjoject/fucapiead/LoginActivity.kt
+++ app/src/main/java/com/davidpjoject/fucapiead/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.davidpjoject.fucapiead
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener{
+            val mIntent = Intent(this, MainActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }
     }
+
 }
